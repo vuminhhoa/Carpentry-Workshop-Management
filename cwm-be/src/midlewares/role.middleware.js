74efc1386dcf48ec269087d3bd92774b1f77942e
Hoa Vu <hoavm.dev@gmail.com>
2023-11-18 20:19:48 +0700

    update middleware

--- cwm-be/src/midlewares/role.middleware.js
+++ cwm-be/src/midlewares/role.middleware.js
@@ -7,22 +7,17 @@ const role = (role) => {
   return (req, res, next) => {
     try {
       const user = req.user;
-      if(user.role_id !== role) return errorHandler(res, err.NOT_AUTHORIZED);
+      if (user.role_id !== role) return errorHandler(res, err.NOT_AUTHORIZED);
       req.user = user;
       next();
-    } catch(error) {
+    } catch (error) {
       return errorHandler(res, error);
     }
-  }
-}
+  };
+};
 
 module.exports = {
   isAdmin: role(RoleSystem.ADMIN),
-  isNvkp: role(RoleSystem.NVKP),
-  isNvpvt: role(RoleSystem.NVPVT),
-  isDdt: role(RoleSystem.DDT),
-  isGd: role(RoleSystem.GD),
-  isTk: role(RoleSystem.TK),
-  isPtpvt: role(RoleSystem.PTPVT),
+
   isUser: role(RoleSystem.USER),
-}
+};
